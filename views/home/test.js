8981555f0afca06a1b0037b0716c0585ce460a6a
Adam Shaylor <adam.shaylor@pollinate.com>
2018-12-11 10:50:50 -0800

    Found a solution
    Works in the latest versions of IE 11, Safari, Firefox and Chrome.

--- views/home/test.js
+++ views/home/test.js
@@ -1,33 +1,18 @@
-const image = new Image();
+const xhr = new XMLHttpRequest();
+xhr.open('GET', '/faux-api/assets/transform/asset/10009011');
+xhr.responseType = 'blob';
 
-// https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image
-image.crossOrigin = 'use-credentials';
+xhr.addEventListener('load', function() {
+  console.log('Phase 1: PASS. The server returned a redirect and the browser detected the load event.');
 
-image.onload = function() {
-  console.log('Phase 1: PASS. The server returned a redirect and the browser loaded the image from the Location response header.');
+  const fileReader = new FileReader();
 
-  const canvas = document.createElement('canvas');
-  const context = canvas.getContext('2d');
+  fileReader.onload = function(loadEvent) {
+    localStorage.setItem('ie-11-redirect-test', loadEvent.target.result);
+    console.log('Phase 2: PASS. The image was successfully loaded as a Blob, read with a FileReader as a data URL, and stored in local storage.')
+  };
 
-  canvas.width = image.width;
-  canvas.height = image.height;
+  fileReader.readAsDataURL(this.response);
+});
 
-  context.drawImage(image, 0, 0);
-  document.body.appendChild(canvas);
-
-  try {
-    localStorage.setItem('ie-11-redirect-test', canvas.toDataURL('image/png'));
-    console.log('Phase 2: PASS. The image was drawn to a canvas and then stored in local storage, which indicates that the canvas is not tainted.');
-  }
-  catch(error) {
-    console.error('Phase 2: FAIL. The image was drawn to a canvas and then threw an error when attempting to store in local storage, which indicates that the canvas is tainted.');
-    console.error(error);
-  }
-};
-
-image.onerror = function(event) {
-  console.error('Phase 1: FAIL: The server returned a redirect and the browser DID NOT load the image from the Location response header.');
-  console.error(event);
-}
-
-image.src = '/faux-api/assets/transform/asset/10009011';
+xhr.send();
